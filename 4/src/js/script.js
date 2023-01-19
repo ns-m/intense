54911c6b4385256dba1f72c560d5ffd6296698d9
kmv <9117479@gmail.com>
2023-01-19 17:36:03 +0300

    1 3

--- 4/src/js/script.js
+++ 4/src/js/script.js
@@ -1,12 +1,15 @@
 window.addEventListener ('load', function() {	
 	const divGet = document.querySelector('.some');
+	const buttonGet = document.querySelector('.btnAddColor');
+
+	// divGet.addEventListener('click', function(){
+	// 	divGet.innerHTML += -1;
+	// });
 
 	divGet.addEventListener('click', function(){
-		divGet.innerHTML += -1;
+		const colorsForBtn = ['#f00', '#ff0', '#0f0'];
+		let rdmNum = Math.floor(Math.random() * colorsForBtn.length)
+		divGet.style.colorsForBtn = colorsForBtn[rdmNum];
 	});
-
-	divGet.addEventListener('click', function(){
-		divGet.innerHTML += -22;
-	});
-	}
-);
+	
+});
