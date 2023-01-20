941c8c2d53123fb9b9caee244eaa84a512f33fd7
kmv <9117479@gmail.com>
2023-01-20 17:54:16 +0300

    1 hw

--- 9/src/js/script.js
+++ 9/src/js/script.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', function(){
 	
+	const input1 = document.querySelector('.num1');
+	const input2 = document.querySelector('.num2');
+	const btnEquals = document.querySelector('.equals');
+	const resultDiv = document.querySelector('.result');
+
+	btnEquals.addEventListener('click', function(){
+		resultDiv.innerHTML = (parseInt(input1.value) + parseInt(input2.value));
+	});
 		
 });
