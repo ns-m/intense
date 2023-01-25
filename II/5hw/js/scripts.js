ab087577d822f8cf8b42e067216a61a48a1d3c71
kmv <9117479@gmail.com>
2023-01-25 17:56:54 +0300

    25012023 1

--- II/5hw/js/scripts.js
+++ II/5hw/js/scripts.js
@@ -2,12 +2,38 @@ window.addEventListener('load', function(){
 
     const formElements = document.querySelectorAll('.check');
     const btnElements = document.querySelector('.btnSend');
+    const mainForm = document.querySelector('.form');    
     
-    
-    btnElements.addEventListener('click', function(){        
+    mainForm.addEventListener('submit', function(e){        
         
+        let hasError = false;
         
+
+        formElements.forEach(function(item){
+            if(item.value === ''){                
+                item.classList.add('err');
+                disabledBtn()
+            }else{
+                item.classList.remove('err');
+                hasError = true;
+            }
+        });
+        
+
+        if (hasError){
+            e.preventDefault();
+        }
     });
 
-    
+    function disabledBtn(){
+        btnElements.disabled = true;
+    }
+
+    function enabledBtn(){
+        btnElements.disabled = false;
+        item.classList.remove('err');
+    }
+
+    formElements.forEach(el => el.addEventListener('input', enabledBtn));
+        
 });
