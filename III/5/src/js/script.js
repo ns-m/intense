f71c11d566aabaf94c993897a3bed526613e14aa
kmv <9117479@gmail.com>
2023-01-26 17:42:56 +0300

    III hw 1

--- III/5/src/js/script.js
+++ III/5/src/js/script.js
@@ -1,4 +1,4 @@
-HTMLElement.prototype.delegate = function delegate(selector, eventName, handler) {
+HTMLElement.prototype.onlyPetPrjDelegate = function onlyPetPrjDelegate(selector, eventName, handler) {
 	const container = this;
 	container.addEventListener(eventName, function(e){		
 		const elem = e.target.closest(selector);
@@ -14,41 +14,32 @@ window.addEventListener('load', function(){
 	const input1 = document.querySelector('.num1');
 	const input2 = document.querySelector('.num2');
 	const btnEquals = document.querySelector('.equals');
-	const resultDiv = document.querySelector('.result');
-	const operands = document.querySelector('.operands');
-	const selectors = [input1, input2, operands];	
+	let resultDiv = document.querySelector('.result');
+	const operations = document.querySelector('.operations');
+	const selectors = [input1, input2, operations];	
+	const mathOperations = {
+		'+' : (x, y) => x + y,
+		'-' : (x, y) => x - y,
+		'*' : (x, y) => x * y,
+		'/' : (x, y) => x / y,
+	};
 	
 
 	btnEquals.addEventListener('click', function(){		
 
 		let resultNum1 = parseInt(input1.value);
 		let resultNum2 = parseInt(input2.value);
-		
-		switch (operands.value) {
-			case '+':
-				resultDiv.innerHTML = (resultNum1 + resultNum2);
-				break;
-			case '-':
-				resultDiv.innerHTML = (resultNum1 - resultNum2);
-				break;
-			case '*':
-				resultDiv.innerHTML = (resultNum1 * resultNum2);
-				break;
-			default:
-				resultDiv.innerHTML = (resultNum1 / resultNum2);
-				break;
-		}
-
+					
 		selectors.forEach(el => el.dataset.last = el.value);
 		this.disabled = true;
-		
+		resultDiv.innerHTML = mathOperations[operations.value](resultNum1, resultNum2)
 	});	
 
-	calculator.delegate('input, select', 'input', function(){
+	calculator.onlyPetPrjDelegate('input, select', 'input', function(){
 		btnEquals.disabled = false;
 	});
 
-	calculator.delegate('.num1, .num2', 'input', function(){
+	calculator.onlyPetPrjDelegate('.num1, .num2', 'input', function(){
 		this.value = this.value.match(/-?[0-9]+\.?[0-9]*/g)[0];
 	});
 		
