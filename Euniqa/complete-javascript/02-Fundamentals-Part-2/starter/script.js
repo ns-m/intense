5e3ffc69f6ca841a3b0901d9445af8764665231e
KMV <9117479@gmail.com>
2023-02-16 16:24:52 +0300

    16022023 1

--- Euniqa/complete-javascript/02-Fundamentals-Part-2/starter/script.js
+++ Euniqa/complete-javascript/02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 const massMark = 95;
 const heightMark = 1.88;
 const massJohn = 85;
@@ -7,7 +8,28 @@ const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 
 if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}`);
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})`);
 } else {
-  console.log(`Mark's BMI ${BMIJohn} is higher than John's BMI ${BMIMark}`);
+  console.log(`Mark's BMI (${BMIJohn}) is higher than John's BMI (${BMIMark})`);
 }
+
+const yourNumber = parseInt(prompt('What is your favorite number?'));
+//console.log(typeof yourNumber);
+
+if (yourNumber === 23) {
+  console.log(`Cool! ${yourNumber} is amazing number`);
+}
+
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log(`You are able to drive`);
+} else {
+  console.log(`Someone else should drive!`);
+}
+*/
