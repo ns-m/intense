21e7f106efd684c64b259e97f563ea22a3b21a7e
KMV <9117479@gmail.com>
2023-02-25 16:11:33 +0300

    25022023 1

--- Euniqa/complete-javascript/03-Developer-Skills/starter/script.js
+++ Euniqa/complete-javascript/03-Developer-Skills/starter/script.js
@@ -1,8 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) {
+  console.log;
+}
+
+function myString() {
+  return '';
 }
