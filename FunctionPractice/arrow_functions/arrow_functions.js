35e84e8f81277d1e8b451dc0f2e36b4c69050049
KMV <9117479@gmail.com>
2023-03-09 12:25:00 +0300

    arrow functions hw 1

--- FunctionPractice/arrow_functions/arrow_functions.js
+++ FunctionPractice/arrow_functions/arrow_functions.js
@@ -1,3 +1,8 @@
+const x = 99;
+const y = 88;
+const testStringWithSpaces = '    Duis mollit deserunt mollit  '; //27 Symbols without spaces
+const testNumArr = [1, 2, 3, 4, 5, 6];
+
 // Task 1
 // Напишите функцию стрелочную функцию t1, которая аналогична закомментированной функции.
 
@@ -6,6 +11,10 @@
 //     const y = 99;
 //     console.log(x + y);
 // }
+
+document
+  .querySelector('.b-1')
+  .addEventListener('click', () => console.log(x + y));
 
 // Task 2.
 // Напишите стрелочную функцию, которая является аналогом анонимной функции, которая запускается при клике на b-2.
@@ -16,6 +25,10 @@
 //     document.querySelector('.out-2').textContent = x + y;
 // })
 
+document.querySelector('.b-2').addEventListener('click', () => {
+  document.querySelector('.out-2').textContent = x + y;
+});
+
 // Task 3.
 // Напишите функцию стрелочную функцию t3, которая аналогична закомментированной функции.
 
@@ -23,7 +36,11 @@
 //     document.querySelector('.out-3').textContent = a + b;
 // }
 
-// const t3 =
+const t3 = (x, y) => {
+  document.querySelector('.out-3').textContent = x + y;
+};
+
+t3(x + 1, y + 1);
 
 // Task 4
 // Напишите функцию стрелочную функцию t4, которая аналогична закомментированной функции.
@@ -32,7 +49,13 @@
 //     document.querySelector('.out-4').textContent = s.trim().length;
 // }
 
-// const t4 =
+const t4 = (s) => {
+  document.querySelector('.out-4').textContent = s.trim().length;
+};
+
+document.querySelector('.b-4').addEventListener('click', () => {
+  t4(testStringWithSpaces);
+});
 
 // Task 5
 // Напишите функцию стрелочную функцию t5, которая аналогична закомментированной функции.
@@ -41,7 +64,11 @@
 //     return Math.pow(a, b);
 // }
 
-// const t5 =
+const t5 = (a, b) => Math.pow(a, b);
+
+document.querySelector('.b-5').addEventListener('click', () => {
+  document.querySelector('.out-5').textContent = t5(x, y);
+});
 
 // Task 6
 // Напишите функцию стрелочную функцию t6, которая аналогична закомментированной функции.
@@ -50,7 +77,11 @@
 //     return ((a % 2 === 0) ? true : false);
 // }
 
-// const t6
+const t6 = (a) => (a % 2 === 0 ? true : false);
+
+document.querySelector('.b-6').addEventListener('click', () => {
+  document.querySelector('.out-6').textContent = t6(y);
+});
 
 // Task 7
 // Напишите функцию стрелочную функцию t7, которая аналогична закомментированной функции. Все функции должны быть стрелочные.
@@ -64,6 +95,12 @@
 
 // const t7 =
 
+document.querySelector('.b-7').addEventListener('click', () => {
+  document.querySelector('.out-7').textContent = testNumArr.map(
+    (item) => item * 2
+  );
+});
+
 // Task 8
 // Напишите функцию стрелочную функцию t8, которая аналогична закомментированной функции. Все функции должны быть стрелочные.
 
@@ -75,6 +112,12 @@
 // }
 
 // const t8
+
+document.querySelector('.b-8').addEventListener('click', () => {
+  document.querySelector('.out-8').textContent = testNumArr.filter(
+    (item) => item > 4
+  );
+});
 
 // Task 9
 // Напишите функцию стрелочную функцию t9, которая аналогична закомментированной функции. Все функции должны быть стрелочные.
