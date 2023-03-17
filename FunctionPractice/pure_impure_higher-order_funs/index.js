0e51552f855a7b2486f03d723255667ef5ad2367
KMV <9117479@gmail.com>
2023-03-17 20:10:52 +0300

    pure_impure_higher-order_funs 1

--- FunctionPractice/pure_impure_higher-order_funs/index.js
+++ FunctionPractice/pure_impure_higher-order_funs/index.js
@@ -1,1 +1,11 @@
-// Pure функции
+// Pure functions
+
+function squad(num) {
+  return num ** 2;
+}
+
+//Not Pure functions
+
+function squad2(num) {
+  document.querySelector(".out-1").innerHTML = num ** 2;
+}
